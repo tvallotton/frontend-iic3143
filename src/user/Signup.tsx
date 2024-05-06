86045b7c4863c450e7336d37015301174c004a7b
naty <naty@ruuf.solar>
2024-05-06 18:50:51 -0400

    signup additional inputs

--- src/user/Signup.tsx
+++ src/user/Signup.tsx
@@ -4,6 +4,9 @@ import { Link } from "react-router-dom";
 export default function Signup() {
     const [email, setEmail] = useState<string>("");
     const [password, setPassword] = useState<string>("");
+    const [firstName, setFirstName] = useState<string>("");
+    const [lastName, setLastName] = useState<string>("");
+    const [confirmPassword, setConfirmPassword] = useState<string>("");
 
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
@@ -15,8 +18,11 @@ export default function Signup() {
             <div className="w-1/2">
                 <div>
                     <h2 className="mt-6 text-center text-3xl font-extrabold font-title">
-                        ¡Inicia sesión en PagePals!
+                        ¡Crea una cuenta en PagePals!
                     </h2>
+                    <p className="mt-2 text-center text-lg font-body text-gray-500">
+                        Y comienza a explorar junto a nuestra comunidad lectora
+                    </p>
                 </div>
                 <form className='mt-8' onSubmit={handleSubmit}>
                     <input type='hidden' name='remember' defaultValue='true' />
@@ -39,7 +45,43 @@ export default function Signup() {
                         </div>
                     </div>
                     <div className='rounded-md'>
-                        <div className="mb-4">
+                        <div className="mb-6">
+                            <p className="text-left">
+                                Nombre
+                            </p>
+                            <input
+                                id='firstName'
+                                name='firstName'
+                                type='firstName'
+                                autoComplete='firstName'
+                                required
+                                className='w-full px-3 py-2 border border-gray-300 placeholder-gray-300 text-gray-900 rounded focus:outline-none focus:border-dark-blue sm:text-sm'
+                                placeholder='Juan'
+                                value={firstName}
+                                onChange={(e) => setFirstName(e.target.value)}
+                            />
+                        </div>
+                    </div>
+                    <div className='rounded-md'>
+                        <div className="mb-6">
+                            <p className="text-left">
+                                Apellido
+                            </p>
+                            <input
+                                id='lastName'
+                                name='lastName'
+                                type='lastName'
+                                autoComplete='lastName'
+                                required
+                                className='w-full px-3 py-2 border border-gray-300 placeholder-gray-300 text-gray-900 rounded focus:outline-none focus:border-dark-blue sm:text-sm'
+                                placeholder='Perez'
+                                value={lastName}
+                                onChange={(e) => setLastName(e.target.value)}
+                            />
+                        </div>
+                    </div>
+                    <div className='rounded-md'>
+                        <div className="mb-6">
                             <p className="text-left">
                                 Contraseña
                             </p>
@@ -56,15 +98,32 @@ export default function Signup() {
                             />
                         </div>
                     </div>
-                    <p className="mb-8 text-left">¿Olvidaste tu contraseña? Haz <Link to='/signup' className="underline text-main-blue font-medium">click aquí</Link></p>
+                    <div className='rounded-md'>
+                        <div className="mb-8">
+                            <p className="text-left">
+                                Confirmar contraseña
+                            </p>
+                            <input
+                                id='pasword'
+                                name='password'
+                                type='password'
+                                autoComplete='password'
+                                required
+                                className='w-full px-3 py-2 border border-gray-300 placeholder-gray-300 text-gray-900 rounded focus:outline-none focus:border-dark-blue sm:text-sm'
+                                placeholder='**********'
+                                value={confirmPassword}
+                                onChange={(e) => setConfirmPassword(e.target.value)}
+                            />
+                        </div>
+                    </div>
                     <div>
                         <button type='submit' className='mb-2 w-full justify-center py-2 px-4 rounded-md text-white bg-main-blue hover:bg-dark-blue'>
-                        Iniciar sesión
+                            Crear cuenta
                         </button>
                     </div>
                     <div>
                         <button type='submit' className='w-full justify-center py-2 px-4 rounded-md text-black bg-white border border-black hover:bg-gray-100'>
-                            Crear cuenta
+                            ¡Ya tengo una cuenta!
                         </button>
                     </div>
                 </form>
