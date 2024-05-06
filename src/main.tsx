49782b179b75d53336a019f2f9827ff1489906a4
naty <naty@ruuf.solar>
2024-05-06 17:34:20 -0400

    add tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import Routing from './common/Router.tsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Routing from "./common/Router.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Routing />
-  </React.StrictMode>,
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+        <Routing />
+    </React.StrictMode>,
 );
