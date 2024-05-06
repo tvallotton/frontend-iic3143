e33f00d30a29ec0669fb2dc0cf236a6aa093e9c6
naty <naty@ruuf.solar>
2024-05-06 18:44:54 -0400

    create signup component

--- src/common/Router.tsx
+++ src/common/Router.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "../App";
 import Login from "../user/Login";
+import Signup from "../user/Signup";
 
 function Routing() {
     return (
@@ -8,6 +9,7 @@ function Routing() {
             <Routes>
                 <Route path={"/"} element={<App />} />
                 <Route path={"/login"} element={<Login />} />
+                <Route path={"/signup"} element={<Signup />} />
             </Routes>
         </BrowserRouter>
     );
---
+++ src/user/Signup.tsx
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+export default function Signup() {
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        console.log("Email:", email, "Password:", password);
+    };
+
+    return (
+        <div className="min-h-screen flex justify-center font-body font-normal">
+            <div className="w-1/2">
+                <div>
+                    <h2 className="mt-6 text-center text-3xl font-extrabold font-title">
+                        ¡Inicia sesión en PagePals!
+                    </h2>
+                </div>
+                <form className='mt-8' onSubmit={handleSubmit}>
+                    <input type='hidden' name='remember' defaultValue='true' />
+                    <div className='rounded-md'>
+                        <div className="mb-6">
+                            <p className="text-left">
+                                Correo electrónico
+                            </p>
+                            <input
+                                id='email'
+                                name='email'
+                                type='email'
+                                autoComplete='email'
+                                required
+                                className='w-full px-3 py-2 border border-gray-300 placeholder-gray-300 text-gray-900 rounded focus:outline-none focus:border-dark-blue sm:text-sm'
+                                placeholder='ejemplo@gmail.com'
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                        </div>
+                    </div>
+                    <div className='rounded-md'>
+                        <div className="mb-4">
+                            <p className="text-left">
+                                Contraseña
+                            </p>
+                            <input
+                                id='pasword'
+                                name='password'
+                                type='password'
+                                autoComplete='password'
+                                required
+                                className='w-full px-3 py-2 border border-gray-300 placeholder-gray-300 text-gray-900 rounded focus:outline-none focus:border-dark-blue sm:text-sm'
+                                placeholder='**********'
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </div>
+                    </div>
+                    <p className="mb-8 text-left">¿Olvidaste tu contraseña? Haz <Link to='/signup' className="underline text-main-blue font-medium">click aquí</Link></p>
+                    <div>
+                        <button type='submit' className='mb-2 w-full justify-center py-2 px-4 rounded-md text-white bg-main-blue hover:bg-dark-blue'>
+                        Iniciar sesión
+                        </button>
+                    </div>
+                    <div>
+                        <button type='submit' className='w-full justify-center py-2 px-4 rounded-md text-black bg-white border border-black hover:bg-gray-100'>
+                            Crear cuenta
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+}
