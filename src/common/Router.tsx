badbe5afc56620de95683d589c0c3537200dc72b
pacristi <piero.cristi@uc.cl>
2024-05-09 18:38:08 -0400

    feat/publication description view main styling

--- src/common/Router.tsx
+++ src/common/Router.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "../App";
 import Login from "../user/Login";
 import Signup from "../user/Signup";
+import PublicationDescription from "../publication/publicationDescription";
 
 function Routing() {
     return (
@@ -10,6 +11,7 @@ function Routing() {
                 <Route path={"/"} element={<App />} />
                 <Route path={"/login"} element={<Login />} />
                 <Route path={"/signup"} element={<Signup />} />
+                <Route path={"/publications/:publicationId"} element={<PublicationDescription />} />
             </Routes>
         </BrowserRouter>
     );
---
+++ src/publication/publicationDescription.tsx
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import Navbar from "../common/Navbar";
+import Footer from "../common/Footer";
+
+interface Publication {
+    title: string;
+    image: string;
+    price: number;
+    author: string;
+    description: string;
+}
+
+
+
+const PublicationDescription: React.FC = () => {
+    const [publication, setPublication] = useState<Publication | null>(null);
+    const {publicationId} = useParams();
+
+    useEffect(() => {
+        axios.get("http://localhost:8080/publications/" + publicationId)
+            .then((response) => {
+                setPublication(response.data.data);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }, [publicationId]);
+
+    if (!publication) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <>
+            <Navbar />
+            <div className="min-h-screen flex justify-center font-body font-normal">
+                <div className="w-1/2 mt-24 m-24">
+                    <img src={publication.image} alt={publication.title} className="w-full h-full object-cover" />
+                </div>
+                <div className="w-1/2 p-6 mt-12 grid grid-cols-1 gap-4">
+                    <div>
+                        <h2 className="mt-6 text-4xl font-extrabold font-title">
+                            {publication.title}
+                        </h2>
+                        <div className="mt-6">
+                            <p className="text-left text-8xl">
+                                ${publication.price.toLocaleString("es-ES", { minimumFractionDigits: 0 })}
+                            </p>
+                            <p className="text-left mt-12">
+                                <span className="font-bold">Autor@:</span> {publication.author}
+                            </p>
+                            <div className="text-left mt-6" dangerouslySetInnerHTML={{ __html: publication.description.replace(/\n/g, "<br />") }} />
+                        </div>
+                    </div>
+                    <button className="bg-main-blue hover:bg-blue-700 text-white font-bold py-2 px-4 rounded self-start w-3/4">
+                        CONTACTAR VENDEDOR
+                    </button>
+                </div>
+            </div>
+            <Footer />
+        </>
+    );
+};
+
+export default PublicationDescription;
