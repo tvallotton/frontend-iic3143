ba5802805231ee91a96e6dd70d0a11cb10b332d8
pacristi <piero.cristi@uc.cl>
2024-05-14 12:25:35 -0400

    feat(button): created button component

--- src/publication/publicationDescription.tsx
+++ src/publication/publicationDescription.tsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 import Navbar from "../common/Navbar";
 import Footer from "../common/Footer";
+import ButtonComponent from "../common/Button";
 
 interface Publication {
     title: string;
@@ -35,8 +36,8 @@ const PublicationDescription: React.FC = () => {
     return (
         <>
             <Navbar />
-            <div className="min-h-screen flex flex-col md:flex-row justify-center font-body">
-                <div className="w-full md:w-3/4 mt-24 md:m-24">
+            <div className="min-h-screen flex flex-col md:flex-row justify-center font-body mx-4 md:mx-12">
+                <div className="w-full md:w-3/4 mt-24 md:m-6">
                     <img
                         src={publication.image}
                         alt={publication.title}
@@ -44,19 +45,22 @@ const PublicationDescription: React.FC = () => {
                         style={{ aspectRatio: "3 / 4" }}
                     />
                 </div>
-                <div className="w-full md:w-1/2 p-6 mt-12 md:mt-0 grid grid-cols-1 gap-8"> <div>
-                    <h2 className="mt-6 text-3xl font-extrabold font-title text-main-blue"> {publication.title}
-                    </h2>
-                    <div className="mt-6">
-                        <p className="text-left text-2xl font-bold text-gray-800"> ${publication.price.toLocaleString("es-ES", { minimumFractionDigits: 0 })}
-                        </p>
-                        <p className="text-left mt-4 text-gray-600"> <span className="font-bold">Autor@:</span> {publication.author}
-                        </p>
-                        <div className="text-left mt-4 text-gray-600" dangerouslySetInnerHTML={{ __html: publication.description.replace(/\n/g, "<br />") }} />
+                <div className="w-full md:w-1/2 p-6 mt-12 md:mt-0 grid grid-cols-1 gap-8">
+                    <div>
+                        <h2 className="mt-6 text-3xl font-extrabold font-title text-main-blue">
+                            {publication.title}
+                        </h2>
+                        <div className="mt-6">
+                            <p className="text-left text-2xl font-bold text-gray-800">
+                        ${publication.price.toLocaleString("es-ES", { minimumFractionDigits: 0 })}
+                            </p>
+                            <p className="text-left mt-4 text-gray-600">
+                                <span className="font-bold">Autor@:</span> {publication.author}
+                            </p>
+                            <div className="text-left mt-4 text-gray-600" dangerouslySetInnerHTML={{ __html: publication.description.replace(/\n/g, "<br />") }} />
+                        </div>
                     </div>
-                </div>
-                <button className="bg-main-blue hover:bg-blue-700 text-white font-bold py-2 px-4 rounded self-start w-full"> CONTACTAR VENDEDOR
-                </button>
+                    <ButtonComponent text="Contactar vendedor" onClick={() => console.log("Comprando...")} />
                 </div>
             </div>
             <Footer />
