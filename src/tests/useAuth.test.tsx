5b1e7c8ce425c20332734e312e4f3cc7eecfa5ec
naty <naty@ruuf.solar>
2024-06-20 21:08:37 -0400

    test use debounced value

--- src/tests/useAuth.test.tsx
+++ src/tests/useAuth.test.tsx
@@ -1,28 +1,28 @@
-import { render, screen } from '@testing-library/react';
-import { AuthContext } from '../auth/AuthContext'; 
-import { useAuth } from '../auth/useAuth';
-import '@testing-library/jest-dom';
+import { render, screen } from "@testing-library/react";
+import { AuthContext } from "../auth/AuthContext"; 
+import { useAuth } from "../auth/useAuth";
+import "@testing-library/jest-dom";
 
 const TestComponent = () => {
-  const auth = useAuth();
-  return <div>{auth.user ? auth.user.name : 'No user'}</div>;
+    const auth = useAuth();
+    return <div>{auth.user ? auth.user.name : "No user"}</div>;
 };
 
-describe('useAuth hook', () => {
-    it('returns context value', () => {
-      const mockAuthValue = {
-        user: { id: '1', name: 'John Doe', lastName: 'Doe', birthdate: '1990-01-01', email: 'john.doe@example.com' },
-        login: jest.fn(),
-        logout: jest.fn(),
-        token: 'test_token', 
-      };
+describe("useAuth hook", () => {
+    it("returns context value", () => {
+        const mockAuthValue = {
+            user: { id: "1", name: "John Doe", lastName: "Doe", birthdate: "1990-01-01", email: "john.doe@example.com" },
+            login: jest.fn(),
+            logout: jest.fn(),
+            token: "test_token", 
+        };
   
-      render(
-        <AuthContext.Provider value={mockAuthValue}>
-          <TestComponent />
-        </AuthContext.Provider>
-      );
+        render(
+            <AuthContext.Provider value={mockAuthValue}>
+                <TestComponent />
+            </AuthContext.Provider>
+        );
   
-      expect(screen.getByText('John Doe')).toBeInTheDocument();
+        expect(screen.getByText("John Doe")).toBeInTheDocument();
     });
-  });
+});
