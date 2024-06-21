2260143af88a7f463c75d4426a847eb754944543
naty <naty@ruuf.solar>
2024-06-21 00:03:56 -0400

    fix lint

--- src/tests/useDebouncedValue.test.tsx
+++ src/tests/useDebouncedValue.test.tsx
@@ -3,26 +3,26 @@ import { useDebouncedValue } from "../hooks/useDebouncedValue";
 import "@testing-library/jest-dom";
  
 function TestComponent({ inputValue, delay }: { inputValue: string, delay: number }) {
-  const debouncedValue = useDebouncedValue(inputValue, delay);
-  return <div>{debouncedValue}</div>;
+    const debouncedValue = useDebouncedValue(inputValue, delay);
+    return <div>{debouncedValue}</div>;
 }
 
 describe("useDebouncedValue in a component", () => {
-  jest.useFakeTimers();
+    jest.useFakeTimers();
 
-  it("should return the same value after the specified delay", async () => {
-    const { rerender } = render(<TestComponent inputValue="initial" delay={500} />);
-    expect(screen.getByText("initial")).toBeInTheDocument();
-    rerender(<TestComponent inputValue="updated" delay={500} />);
-    act(() => {
-      jest.advanceTimersByTime(100);
+    it("should return the same value after the specified delay", async () => {
+        const { rerender } = render(<TestComponent inputValue="initial" delay={500} />);
+        expect(screen.getByText("initial")).toBeInTheDocument();
+        rerender(<TestComponent inputValue="updated" delay={500} />);
+        act(() => {
+            jest.advanceTimersByTime(100);
+        });
+        expect(screen.getByText("initial")).toBeInTheDocument();
+        act(() => {
+            jest.advanceTimersByTime(400);
+        });
+        expect(await screen.findByText("updated")).toBeInTheDocument();
     });
-    expect(screen.getByText("initial")).toBeInTheDocument();
-    act(() => {
-      jest.advanceTimersByTime(400);
-    });
-    expect(await screen.findByText("updated")).toBeInTheDocument();
-  });
 
-  jest.useRealTimers();
+    jest.useRealTimers();
 });
